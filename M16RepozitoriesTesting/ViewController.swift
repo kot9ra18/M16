8b21b8465b1df158ced6e14897b1e7c4221d837c
kot9ra18 <alexandr-prokhorov@mail.ru>
2023-06-13 20:13:20 +0500

    First comit

---
+++ M16RepozitoriesTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16RepozitoriesTesting
+//
+//  Created by Александр Прохоров on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
